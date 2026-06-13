20b0565d87b822645a61d06360ae1df9d9eec1af
LeadCardinal <cardinaltriune@gmail.com>
2026-06-13 12:38:08 -0500

    Add .gitignore; untrack node_modules and dist (build artifacts should not be versioned)

--- dist/assets/arrow-left-d7970351.js
+++
@@ -1,2 +0,0 @@
-import{c as e}from"./index-d073a5dd.js";const t=e("ArrowLeft",[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]]);export{t as A};
-//# sourceMappingURL=arrow-left-d7970351.js.map
